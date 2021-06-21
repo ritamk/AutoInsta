0b6dcd747ee920ed1b13a4a7f2f9d6b311dc1a9c
ritamk <70438561+ritamk@users.noreply.github.com>
2021-06-21 18:11:14 +0530

    Update InstaMain.py

--- InstaMain.py
+++ InstaMain.py
@@ -1,36 +1,5 @@
-from time import sleep
-from selenium import webdriver
-import InstaDef
+from InstaDef import do as I
 
-# initialises the browser.
-browser = webdriver.Firefox()
-browser.get('https://www.instagram.com/')
-browser.implicitly_wait(5)
-
-# variable initialisation.
-i = 0
-profLikeCount = 20
-homLikeCount = 5
-sugFollowCount = 9
-
-# running the task.
-loggingIn = InstaDef.loginPage(browser)
-feed = loggingIn.login("username", "password")
-feed.sayNo()
-handle = feed.user("ritamwho")
-handle.follow()
-if (handle.private() == 0):
-    sleep(30)
-    browser.refresh()
-handle.postSelect()
-while (i < int(handle.postCount()) and i < profLikeCount):
-    handle.postLike()
-    handle.next()
-    i += 1
-handle.close()
-sleep(2)
-handle.feed()
-
-# waiting before quitting session.
-sleep(20)
-browser.quit()
+I.startIG()
+I.login('username', 'password')
+I.stopIG(5)
