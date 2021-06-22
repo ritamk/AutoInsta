2664093f09ca4dbe38e08a8f59dcbca942271a80
ritamk <70438561+ritamk@users.noreply.github.com>
2021-06-22 23:14:18 +0530

    Update InstaDef.py

--- InstaDef.py
+++ InstaDef.py
@@ -9,7 +9,7 @@ browser = webdriver.Firefox()
 
 # class for login page actions.
 class loginPage:
-    # logs in with provided username, password and returns to feedPage (next page after loginPage).
+    # logs in with provided username, password and returns to feed (next page after loginPage).
     def login(username, password):
         browser.implicitly_wait(5)
         usernameInput = browser.find_element_by_css_selector("input[name = 'username']")
@@ -20,10 +20,9 @@ class loginPage:
         passwordInput.send_keys(Keys.ENTER)
         # loginTap = self.browser.find_element_by_xpath("//div[text() = 'Log In']")
         # loginTap.click()
-        return feedPage()
 
 # class for feed page actions.
-class feedPage:
+class feed:
     # selects "No" when prompted if the browser should remember the login details.
     def sayNo():
         sleep(1)
@@ -42,41 +41,43 @@ class feedPage:
         noNotif = browser.find_element_by_xpath("//button[contains(.,'Not Now')]")
         noNotif.click()
 
-    # # goes to home page.
-    # def home(self):
-    #     sleep(2)
-    #     profPage = self.browser.get('https://www.instagram.com/')
-    #     return do(self.browser)
-    #
-    # # goes to my profile page.
-    # def profile(self):
-    #     sleep(2)
-    #     profPage = self.browser.get('https://www.instagram.com/ritamksth/')
-    #     return do(self.browser)
-    #
-    # # goes to profile page of specified user.
-    # def user(self, name):
-    #     sleep(2)
-    #     profPage = self.browser.get('https://www.instagram.com/{}/'.format(name))
-    #     return do(self.browser)
-    #
-    # # goes to the explore page.
-    # def explore(self):
-    #     sleep(2)
-    #     tagPage = self.browser.get('https://www.instagram.com/explore/')
-    #     return do(self.browser)
-    #
-    # # goes to the explore page of specified tag.
-    # def tag(self, tag):
-    #     sleep(2)
-    #     tagPage = self.browser.get('https://www.instagram.com/explore/tags/{}/'.format(tag))
-    #     return do(self.browser)
-    #
-    # # goes to the suggested people to follow page.
-    # def suggestion(self):
-    #     sleep(2)
-    #     sugnPage = self.browser.get('https://www.instagram.com/explore/people/suggested/')
-    #     return do(self.browser)
+    # goes to home page.
+    def home():
+        sleep(1)
+        profPage = browser.find_element_by_xpath("//*[name() = 'svg'][@aria-label = 'Home']")
+        profPage.click()
+
+    # goes to my profile page.
+    def profile():
+        sleep(1)
+        profPage = browser.find_element_by_xpath("//img[@alt = contains(.,'profile picture')]")
+        profPage.click()
+
+    # goes to profile page of specified user.
+    def user(name):
+        sleep(1)
+        browser.get('https://www.instagram.com/{}/'.format(name))
+
+    # goes to the explore page.
+    def explore():
+        sleep(1)
+        expPage = browser.find_element_by_xpath("//*[name() = 'svg'][@aria-label = 'Find People']")
+        expPage.click()
+
+    # goes to the explore page of specified tag.
+    def tag(tag):
+        sleep(1)
+        browser.get('https://www.instagram.com/explore/tags/{}/'.format(tag))
+
+    # goes to the suggested people to follow page.
+    def suggestion():
+        sleep(1)
+        browser.get('https://www.instagram.com/explore/people/suggested/')
+
+    # goes to the top profiles page.
+    def topAcc():
+        sleep(1)
+        browser.get('https://www.instagram.com/directory/profiles/0-0/')
 
 # performs functions the user specifies
 class do:
@@ -84,8 +85,8 @@ class do:
         browser.get('https://www.instagram.com/')
 
     def login(username, password):
-        prompt = loginPage.login('{}'.format(username), '{}'.format(password))
-        prompt.sayNo()
+        loginPage.login('{}'.format(username), '{}'.format(password))
+        feed.sayNo()
 
     def stopIG(wait):
         sleep(wait)
@@ -100,20 +101,82 @@ class do:
         action.scroll('top')
 
     def suggestedFollow(num):
-        sug = browser.get('https://www.instagram.com/explore/people/suggested')
+        feed.suggestion()
         for i in range(0, num):
             action.sugFollow()
         action.feed()
 
+    def profUnfollow(numprof):
+
+
     def exploreComment(num, text):
-        exp = browser.get('https://www.instagram.com/explore/')
+        feed.explore()
         action.expSelect()
         for i in range(0, num):
-            com = random.choice(text)
-            action.comment(com)
+            action.comment(random.choice(text))
             action.next()
         action.close()
-        action.feed()
+        feed.home()
+
+    def exploreToProfile(num, numprof, text):
+        for i in range(0, numprof):
+            feed.explore()
+            action.expSelect()
+            sleep(1)
+            for k in range(0, i):
+                action.next()
+            action.postToProf()
+            action.postSelect()
+            for j in range(0, num):
+                action.postLike()
+                action.comment(random.choice(text))
+                action.next()
+            action.close()
+
+    def tagTopComment(hash, num, text):
+        feed.tag(hash)
+        action.topSelect()
+        for i in range(0, num):
+            action.comment(random.choice(text))
+            action.next()
+        action.close()
+        feed.home()
+
+    def tagRecComment(hash, num, text):
+        feed.tag(hash)
+        action.recSelect()
+        for i in range(0, num):
+            action.postLike()
+            action.comment(random.choice(text))
+            action.next()
+        action.close()
+        feed.home()
+
+    def tagToProfile(hash, num, numprof, text):
+        for i in range(0, numprof):
+            feed.tag(hash)
+            action.topSelect()
+            sleep(1)
+            for k in range(0, i):
+                action.next()
+            action.postToProf()
+            action.postSelect()
+            for j in range(0, num):
+                action.postLike()
+                action.next()
+            action.comment(random.choice(text))
+            action.close()
+
+    def topProfile(num, numprof, text):
+        for i in range(1, numprof + 1):
+            feed.topAcc()
+            action.topAccounts(i)
+            for j in range(0, num):
+                action.postSelect()
+                sleep(1)
+                action.comment(random.choose(text))
+                action.next()
+            action.close()
 
 # class for basic instagram interactions.
 class action:
@@ -152,12 +215,12 @@ class action:
     # clicks the unfollow button in own following page.
     def profUnfollow():
         sleep(2)
-        following = unfollow = browser.find_element_by_xpath("//button[text() = 'Following']")
+        unfollow = browser.find_element_by_xpath("//button[text() = 'Following']")
         following.click()
         sleep(1)
         unfollow = browser.find_element_by_xpath("//button[text() = 'Unfollow']")
         # coordinates = unfollow.location_once_scrolled_into_view
-        # self.browser.execute_script('window.scrollTo({}, {});'.format(coordinates['x'], coordinates['y']))
+        # browser.execute_script('window.scrollTo({}, {});'.format(coordinates['x'], coordinates['y']))
         unfollow.click()
 
     # clicks the like icon in home page.
@@ -165,8 +228,6 @@ class action:
         for count in range(1, n + 1):
             sleep(4)
             like = browser.find_element_by_xpath("//*[name() = 'svg'][@aria-label = 'Like'][@height = '24']")
-            # coordinates = like.location_once_scrolled_into_view
-            # self.browser.execute_script('window.scrollTo({}, {});'.format(coordinates['x'], coordinates['y']))
             like.click()
 
     # clicks the dislike icon in home page.
@@ -174,29 +235,50 @@ class action:
         for count in range(1, n + 1):
             sleep(4)
             dislike = browser.find_element_by_xpath("//*[name() = 'svg'][@aria-label = 'Unlike'][@height = '24']")
+            # coordinates = dislike.location_once_scrolled_into_view
+            # browser.execute_script('window.scrollTo({}, {});'.format(coordinates['x'], coordinates['y']))
+            dislike.click()
 
     # clicks the like button in posts.
     def postLike():
         sleep(1)
-        like = browser.find_element_by_xpath("//*[name() = 'svg'][@aria-label = 'Like'][@height = '24']")
-        like.click()
+        try:
+            like = browser.find_element_by_xpath("//*[name() = 'svg'][@aria-label = 'Like'][@height = '24']")
+            like.click()
+        except NoSuchElementException:
+            pass
 
     # clicks the dislike button in posts.
     def postDislike():
         sleep(1)
-        dislike = browser.find_element_by_xpath("//*[name() = 'svg'][@aria-label = 'Unlike'][@height = '24']")
-        dislike.click()
+        try:
+            dislike = browser.find_element_by_xpath("//*[name() = 'svg'][@aria-label = 'Unlike'][@height = '24']")
+            dislike.click()
+        except NoSuchElementException:
+            pass
 
     # comments on posts.
     def comment(text):
         sleep(2)
         try:
+            sleep(1)
             comment = browser.find_element_by_xpath("//textarea")
             comment.click()
             comment = browser.find_element_by_xpath("//textarea")
             comment.send_keys(text)
             comment.send_keys(Keys.ENTER)
         except NoSuchElementException:
+            sleep(1)
+            pause = browser.find_element_by_xpath("//div[@aria-label = 'Control']")
+            pause.click()
+            sleep(1)
+            comment = browser.find_element_by_xpath("//textarea")
+            comment.click()
+            sleep(1)
+            comment = browser.find_element_by_xpath("//textarea")
+            comment.send_keys(text)
+            comment.send_keys(Keys.ENTER)
+        except:
             pass
 
     # clicks the first picture in the profile page.
@@ -233,19 +315,25 @@ class action:
 
     # clicks the next button on a post in a profile.
     def next():
-        sleep(1)
-        try:
-            next = browser.find_element_by_xpath("//a[text() = 'Next']")
-            next.click()
-            return 0
-        except NoSuchElementException:
-            return 1
+        sleep(2)
+        # try:
+        next = browser.find_element_by_xpath("//a[contains(.,'Next')]")
+        next.click()
+        return 0
+        # except NoSuchElementException:
+        #     return 1
+        #     pass
 
     # clicks the previous button on a post in a profile.
     def prev():
-        sleep(1)
-        next = browser.find_element_by_xpath("//a[text() = 'Previous']")
-        next.click()
+        sleep(2)
+        try:
+            prev = browser.find_element_by_xpath("//a[contains(.,'Next')]")
+            prev.click()
+            return 0
+        except NoSuchElementException:
+            return 1
+            pass
 
     # clicks the close button on a post in a profile.
     def close():
@@ -256,12 +344,6 @@ class action:
         except NoSuchElementException:
             pass
 
-    # clicks the home button.
-    def feed():
-        sleep(1)
-        home = browser.find_element_by_xpath("//*[name() = 'svg'][@aria-label = 'Home']")
-        home.click()
-
     # returns number of posts of user.
     def postCount():
         sleep(1)
@@ -269,9 +351,16 @@ class action:
         num = BS(posts.get_attribute('innerHTML'), 'html.parser').text
         return num
 
+    # scrolls to the top if specified, otherwise goes 400px downwards.
     def scroll(dir):
         sleep(1)
         if (dir == 'top'):
             browser.execute_script('window.scrollTo(0, 0);')
         else:
             browser.execute_script('window.scrollTo(0, 400);')
+
+    # clicks the top profiles sequentially
+    def topAccounts(num):
+        sleep(1)
+        prof = browser.find_element_by_xpath("//li[num]/a")
+        prof.click()
